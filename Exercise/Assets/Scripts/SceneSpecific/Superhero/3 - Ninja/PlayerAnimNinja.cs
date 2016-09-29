7ee5e868055f7b9bc512bbaab772e56a0ac62368
Dezypher <lionkidzap@gmail.com>
2016-09-29 22:54:09 +0800

    New Scenes and Fonts
    + New Fonts
    + Ninja Dojo Scene for Half/Whole Side Planking Exercises

---
+++ Exercise/Assets/Scripts/SceneSpecific/Superhero/3 - Ninja/PlayerAnimNinja.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimNinja : MonoBehaviour {
+
+	private GameLogic gameLogic;
+	private Animator animator;
+	private bool wait = false;
+	private UnityEngine.UI.Image fade;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		gameLogic = GameObject.Find ("GameHandler").GetComponent<GameLogic> ();
+		fade = GameObject.Find ("Fade").GetComponent<UnityEngine.UI.Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameLogic.done && gameLogic.started && !wait) {
+			if (fade.color.a == 1) {
+				wait = true;
+				animator.SetTrigger ("NextPhase");
+			}
+		}
+
+		if (!gameLogic.started) {
+			wait = false;
+		}
+	}
+}
