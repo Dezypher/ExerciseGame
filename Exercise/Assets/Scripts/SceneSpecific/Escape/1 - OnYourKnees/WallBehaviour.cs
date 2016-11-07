13ab68f3e0bab373f8b9c08f14cd694505433155
Dezypher <lionkidzap@gmail.com>
2016-11-07 20:54:12 +0800

    Added base for Escape 1 - On Your Knees

---
+++ Exercise/Assets/Scripts/SceneSpecific/Escape/1 - OnYourKnees/WallBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallBehaviour : MonoBehaviour {
+
+	public float xCoordPlayerPos;
+	public float xCoordDelete;
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position =
+			new Vector3 (
+				transform.position.x + (speed * Time.deltaTime),
+				transform.position.y,
+				transform.position.z
+			);
+
+		if (transform.position.x <= xCoordDelete) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Exercise/Assets/Scripts/SceneSpecific/Escape/1 - OnYourKnees/WallSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallSpawner : MonoBehaviour {
+
+	public GameObject wall;
+	public float intervalMin;
+	public float intervalMax;
+
+	public float timeTillNext = 0;
+
+	public int numToSpawn;
+
+	public int numSpawned;
+
+	void Start () { 
+		timeTillNext = Random.Range (intervalMin, intervalMax);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (numSpawned < numToSpawn) {
+			timeTillNext -= Time.deltaTime;
+
+			if (timeTillNext <= 0) {
+				Instantiate (wall, transform.position, transform.rotation);
+				numSpawned++;
+				timeTillNext = Random.Range (intervalMin, intervalMax);
+			}
+		}
+	}
+}
