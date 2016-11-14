40e06e281066f0fb0f6bdbaa9b9dd19e642e9ae9
Dezypher <ccs@CCSs-iMac.local>
2016-11-14 12:41:11 +0800

    Started Wall Dodge Scene

---
+++ Exercise/Assets/Scripts/Game/Spawner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject toSpawn;
+
+	public void Spawn() {
+		Instantiate (toSpawn, transform.position, transform.rotation);
+	}
+}
---
+++ Exercise/Assets/Scripts/SceneSpecific/Escape/2 - Wall Dodge/DualWallSpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DualWallSpawner : MonoBehaviour {
+
+	public Spawner leftSpawner;
+	public Spawner rightSpawner;
+
+	public float intervalMax;
+	public float intervalMin;
+
+	public float currTime;
+	public int amtToSpawn;
+
+	private GameLogic gameLogic;
+
+	//To make sure both sides spawn an equal number of walls
+	private int leftSpawned;
+	private int rightSpawned;
+
+	void Start() {
+		gameLogic = GameObject.Find ("GameHandler").GetComponent<GameLogic> ();
+	}
+
+	void Update () {
+		if (gameLogic.started && !gameLogic.done && amtToSpawn < (leftSpawned + rightSpawned)) {
+			currTime -= Time.deltaTime;
+
+			if (currTime <= 0) {
+				int side = Random.Range (0, 1);
+
+				if (side == 0) {
+					if (leftSpawned < amtToSpawn / 2) {
+						leftSpawner.Spawn ();
+						leftSpawned++;
+					} else {
+						rightSpawner.Spawn ();
+						rightSpawned++;
+					}
+				} else {
+					if (rightSpawned < amtToSpawn / 2) {
+						rightSpawner.Spawn ();
+						rightSpawned++;
+					} else {
+						leftSpawner.Spawn ();
+						leftSpawned++;
+					}
+				}
+			}
+		}
+	}
+}
