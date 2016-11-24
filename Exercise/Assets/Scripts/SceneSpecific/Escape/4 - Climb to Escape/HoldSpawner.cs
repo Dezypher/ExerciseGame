902f9b27c53c9fc634ae17533a7ee9abb7c9820d
Dezypher <ccs@CCSs-iMac.local>
2016-11-24 13:43:12 +0800

    Finalized Wall Climb

---
+++ Exercise/Assets/Scripts/SceneSpecific/Escape/4 - Climb to Escape/HoldSpawner.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldSpawner : MonoBehaviour {
+
+	public const int LEFT = 0;
+	public const int RIGHT = 1;
+
+	public GameObject holdPrefab;
+	public int amountToSpawn;
+
+	public float yMargin;
+	public float xMargin;
+
+	public int spawnedLeft = 0;
+	public int spawnedRight = 0;
+
+	public GameObject[] holds;
+	public int[] sequence;
+
+	void Start () {
+		holds = new GameObject[amountToSpawn];
+		sequence = new int[amountToSpawn];
+
+		for (int i = 0; i < amountToSpawn; i++) {
+			int side = Random.Range (0, 2);
+
+			int xMod = 0;
+
+			if (side == 0) {
+				if (spawnedLeft < amountToSpawn / 2) {
+					xMod = -1;
+					spawnedLeft++;
+				} else {
+					xMod = 1;
+					spawnedRight++;
+				}
+			} else {
+				if (spawnedRight < amountToSpawn / 2) {
+					xMod = 1;
+					spawnedRight++;
+				} else {
+					xMod = -1;
+					spawnedLeft++;
+				}
+			}
+
+			Vector3 pos = new Vector3 (
+				transform.position.x + (xMargin * xMod),			              
+				transform.position.y + (yMargin * i),
+				transform.position.z
+			);
+
+			GameObject hold = (GameObject) Instantiate (holdPrefab, pos, holdPrefab.transform.rotation);
+
+			if (xMod == -1)
+				sequence [i] = LEFT;
+			else
+				sequence [i] = RIGHT;
+
+			holds [i] = hold;
+		}
+	}
+}
---
+++ Exercise/Assets/Scripts/SceneSpecific/Escape/4 - Climb to Escape/PlayerClimb.cs
@@ -0,0 +1,75 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerClimb : MonoBehaviour {
+	
+	public Animator animator;
+	public HoldSpawner holdSpawner;
+	public GameLogic gameLogic;
+
+	public int amountClimbed = 0;
+
+	public Vector3 target;
+
+	public bool climbing = false;
+
+	public float speed;
+
+	public float holdTimeLeft;
+	public float timeHeldLeft;
+	public float holdTimeRight;
+	public float timeHeldRight;
+
+	void Start () {
+		holdSpawner = GameObject.Find ("HoldSpawner").GetComponent<HoldSpawner> ();
+		animator = GetComponent<Animator> ();
+		gameLogic = GameObject.Find ("GameHandler").GetComponent<GameLogic> ();
+	}
+
+	void Update () {
+		//if (gameLogic.started && !gameLogic.done) {
+
+			transform.position = Vector3.MoveTowards (transform.position, target, speed * Time.deltaTime);
+
+			if (Input.GetKey (KeyCode.A) && !climbing) {
+				if (holdSpawner.sequence [amountClimbed] == HoldSpawner.LEFT) {
+					if (timeHeldLeft < holdTimeLeft) {
+						timeHeldLeft += Time.deltaTime;
+					} else {
+						timeHeldLeft = 0;
+						animator.SetTrigger ("Left");
+						StartCoroutine(Climb ());
+					}
+				}
+			} else if (Input.GetKey (KeyCode.D) && !climbing) {
+				if (holdSpawner.sequence [amountClimbed] == HoldSpawner.RIGHT) {
+					if (timeHeldRight < holdTimeRight) {
+						timeHeldRight += Time.deltaTime;
+					} else {
+						timeHeldRight = 0;
+						animator.SetTrigger ("Right");
+						StartCoroutine(Climb ());
+					}
+				}
+			}
+		//}
+	}
+
+	public IEnumerator Climb() {
+		Vector3 hold = holdSpawner.holds [amountClimbed].transform.position;
+		Vector3 holdTarget = 
+			new Vector3 (
+				hold.x,
+				hold.y - 3.5f,
+				hold.z - 1.5f
+			);
+
+		target = holdTarget;
+		amountClimbed++;
+
+		climbing = true;
+		yield return new WaitForSeconds (0.5f);
+		climbing = false;
+	}
+}
